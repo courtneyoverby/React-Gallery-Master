ebc8be7d4f9e6fd28f47d4c986377e710e4fcc1a
Courtney Overby <brioverby@gmail.com>
2020-04-12 21:20:34 -0500

    App.js gallery.daata.js is set up, without likeImage method, and props

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import "./App.css";
 import axios from "axios";
-import GalleryList from "../GalleryList/GalleryList";
+// import GalleryList from "../GalleryList/GalleryList";
 
 class App extends Component {
   constructor(props) {
@@ -18,25 +18,14 @@ class App extends Component {
   }
 
   getGallery() {
-    axios("/gallery").then((passGallery) => {
+    axios("/gallery").then((sendGallery) => {
       this.setState({
-        galleryCollection: passGallery.data,
+        galleryCollection: sendGallery.data,
       });
-      return passGallery;
+      return sendGallery;
     });
   }
 
-  onLikeImage = (imageId) => {
-    axios
-      .put(`/gallery/like/${imageId}`)
-      .then((passLike) => {
-        console.log("passLike: ", passLike);
-        this.fetchGallery();
-      })
-      .catch((failLike) => {
-        console.error("Error liking the image: ", failLike);
-      });
-  };
 
   render() {
     return (
@@ -46,17 +35,14 @@ class App extends Component {
             Salvador Dali Artwork - Surrealism Movement
           </h1>
         </header>
-        <p class="gallery-bio">
+        <p className="gallery-bio">
           Salvador Dali (11 May 1904 – 23 January 1989) – Avant-garde Spanish
           surrealist painter, film-maker, sculptor, and photographer. Famous
           works include “The Persistence of Memory” which hints at the illusion
           of time. Dali sought to challenge convention through his surrealist
           art.
         </p>
-        <GalleryList
-          collection={this.state.galleryCollection}
-          likeImage={this.onLikeImage}
-        />
+      
       </div>
     );
   }
